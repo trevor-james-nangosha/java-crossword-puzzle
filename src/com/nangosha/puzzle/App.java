d958d95249aa93f5cb2caeb163b98ded59b2a1d1
trevor-james-nangosha <trevornangosha16@gmail.com>
2022-10-27 15:59:05 +0300

    testing out letter allocation

--- src/com/nangosha/puzzle/App.java
+++ src/com/nangosha/puzzle/App.java
@@ -3,9 +3,9 @@ package com.nangosha.puzzle;
 public class App {
     public static void main(String[] args) {
         CrosswordPuzzle puzzle = new CrosswordPuzzle(17, 19);
-        Grid[] row = puzzle.gridMatrix[2];
+        Grid[] row = puzzle.gridMatrix[3];
         Grid thirdGrid = row[18];
-        System.out.println(thirdGrid.letter);
+        System.out.println(thirdGrid.letter); // testing out letter allocation
     }
 }
 
